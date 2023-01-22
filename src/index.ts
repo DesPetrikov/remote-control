e7fcb6936f9e73d33fac4210b8ef5fe846817437
Dzianis Petrykau <desik1603@gmail.com>
2023-01-22 23:49:43 +0300

    feat: navigation block was implemented

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import { httpServer } from './http_server/index.js';
-import { mouse } from '@nut-tree/nut-js';
+import { Button, down, left, mouse, Region, right, straightTo, up } from '@nut-tree/nut-js';
+import { WebSocketServer } from 'ws';
+import { Commands } from './types.js';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Connection accepted');
+  ws.on('message', async (data) => {
+	mouse.config.mouseSpeed = 200;
+    const [instruction, ...values] = String(data).split(' ');
+    const { x: posX, y: posY } = await mouse.getPosition();
+    if (instruction === Commands.UP) {
+      await mouse.move(straightTo({ x: posX, y: posY - +values[0] }));
+    } else if (instruction === Commands.DOWN) {
+      await mouse.move(straightTo({ x: posX, y: posY + +values[0] }));
+    } else if (instruction === Commands.LEFT) {
+      await mouse.move(straightTo({ x: posX - +values[0], y: posY }));
+    } else if (instruction === Commands.RIGHT) {
+      await mouse.move(straightTo({ x: posX + +values[0], y: posY }));
+    } else if (instruction === Commands.MOUSE_POSITION) {
+      ws.send(`mouse_position ${posX},${posY}`);
+    }
+  });
+});
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export enum Commands {
+	UP = 'mouse_up',
+	DOWN = 'mouse_down',
+	LEFT = 'mouse_left',
+	RIGHT = 'mouse_right',
+	MOUSE_POSITION = 'mouse_position',
+	CIRCLE = 'draw_circle',
+	SQUARE = 'draw_square',
+	RECTANGLE = 'draw_rectangle',
+
+}
