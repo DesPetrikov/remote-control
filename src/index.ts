101bcf0c6b77b670873915d5f92f1915ab19e96c
Dzianis Petrykau <desik1603@gmail.com>
2023-01-23 14:08:46 +0300

    feat: drawing block was implemented

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', (ws) => {
   console.log('Connection accepted');
   ws.on('message', async (data) => {
-	mouse.config.mouseSpeed = 200;
     const [instruction, ...values] = String(data).split(' ');
     const { x: posX, y: posY } = await mouse.getPosition();
     if (instruction === Commands.UP) {
@@ -26,6 +25,30 @@ wss.on('connection', (ws) => {
       await mouse.move(straightTo({ x: posX + +values[0], y: posY }));
     } else if (instruction === Commands.MOUSE_POSITION) {
       ws.send(`mouse_position ${posX},${posY}`);
+    } else if (instruction === Commands.SQUARE) {
+      await mouse.pressButton(Button.LEFT);
+      await mouse.move(right(+values[0]));
+      await mouse.move(down(+values[0]));
+      await mouse.move(left(+values[0]));
+      await mouse.move(up(+values[0]));
+      await mouse.releaseButton(Button.LEFT);
+    } else if (instruction === Commands.RECTANGLE) {
+      await mouse.pressButton(Button.LEFT);
+      await mouse.move(right(+values[0]));
+      await mouse.move(down(+values[1]));
+      await mouse.move(left(+values[0]));
+      await mouse.move(up(+values[1]));
+      await mouse.releaseButton(Button.LEFT);
+    } else if (instruction === Commands.CIRCLE) {
+      const { x: startX, y: startY } = await mouse.getPosition();
+      const [circleCenterX, circleCenterY] = [startX + +values[0], startY];
+      await mouse.pressButton(Button.LEFT);
+      for (let i = 0; i <= 360; i++) {
+			const Y = +values[0] * Math.sin(i * Math.PI / 180)
+			const X = +values[0] * Math.cos(i * Math.PI / 180)
+        await mouse.move(straightTo({ x: circleCenterX - X, y: circleCenterY - Y }));
+      }
+      await mouse.releaseButton(Button.LEFT);
     }
   });
 });
